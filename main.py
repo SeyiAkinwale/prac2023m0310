28f158ee8a95f95e281e093a781cc26b5690fff3
SeyiAkinwale1 <72584469+SeyiAkinwale@users.noreply.github.com>
2023-03-10 19:36:23 +0000

    YES NO

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 horizontal = int(input())
 vertical = int(input())
 sum = horizontal + vertical
-if (sum%2==0):
-  print(True)
+if (sum % 2 == 0):
+  print("YES")
 else:
-  print(False)
+  print("NO")
